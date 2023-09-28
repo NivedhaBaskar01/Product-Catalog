35910b061842445e0afe1cde905b9e21685d269a
NivedhaBaskar01 <nivedha.b@prodapt.com>
2023-09-28 15:40:40 +0530

    second commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { UserserviceService } from '../userservice.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit
+{
+  model:any={}
+  getData:any
+  constructor(private userservice:UserserviceService,private router:Router)
+  {}
+  ngOnInit(): void 
+  {
+
+  }
+  loginuser()
+  {
+    var user=this.model.username;
+    var password=this.model.password;
+    this.userservice.getUserData(user,password).subscribe((res:any)=>
+    {
+      this.getData=res;
+      if(this.getData==true)
+      {
+        this.router.navigate(['/home'])
+      }
+      else{
+        alert("Invalid credentials");
+      }
+    })
+  }
+
+}
---
+++ src/app/userservice.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserserviceService {
+
+  constructor(private http:HttpClient) 
+  { 
+
+  }
+  getUserData(username:String,password:String)
+  {
+    return this.http.get('http://localhost:8080/user/'+username+'/'+password);
+  }
+}
